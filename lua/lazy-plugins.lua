e8b62dae93ce55fdc1ee9981a83c5f56c03d6350
Moritz <moritz.boeker@node-robotics.com>
2025-09-16 12:36:25 +0200

    feat: Add Avante

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -23,33 +23,21 @@ require('lazy').setup({
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart/plugins/gitsigns',
-
-  require 'kickstart/plugins/which-key',
-
-  require 'kickstart/plugins/telescope',
-
-  require 'kickstart/plugins/lspconfig',
-
-  require 'kickstart/plugins/conform',
-
-  require 'kickstart/plugins/cmp',
-
-  require 'kickstart/plugins/tokyonight',
-
-  require 'kickstart/plugins/todo-comments',
-
-  require 'kickstart/plugins/mini',
-
-  require 'kickstart/plugins/treesitter',
-
-  require 'kickstart/plugins/neogit',
-
   require 'kickstart/plugins/autosave',
-
+  require 'kickstart/plugins/avante',
+  require 'kickstart/plugins/cmp',
+  require 'kickstart/plugins/conform',
+  require 'kickstart/plugins/gitsigns',
+  require 'kickstart/plugins/lspconfig',
+  require 'kickstart/plugins/mini',
+  require 'kickstart/plugins/neogit',
   require 'kickstart/plugins/oil',
-
   require 'kickstart/plugins/surround',
+  require 'kickstart/plugins/telescope',
+  require 'kickstart/plugins/todo-comments',
+  require 'kickstart/plugins/tokyonight',
+  require 'kickstart/plugins/treesitter',
+  require 'kickstart/plugins/which-key',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
